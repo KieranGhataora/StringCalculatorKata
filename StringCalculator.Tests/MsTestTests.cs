34344254429fefcf976ac7daba9d8d198efc7961
Robert Stanley <stanleyra@aol.com>
2021-01-27 14:04:17 +0000

    final attempt

--- StringCalculator.Tests/MsTestTests.cs
+++ StringCalculator.Tests/MsTestTests.cs
@@ -1,14 +1,42 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using StringCalculator;
 namespace StringCalculator.Tests
 {
     [TestClass]
     public class MsTestTests
     {
         [TestMethod]
-        public void TestEmptyString()
+        public void CalcAdd_TestEmptyString()
         {
-            Assert.IsTrue(false);
+            Calculator clc  =  new Calculator();
+            int retVal= clc.Add(string.Empty);
+            const int  constZero = 0;
+            Assert.AreEqual(retVal, constZero, "Add calculator for empty String");
+
+        }
+
+       
+
+
+        [TestMethod]
+        public void CalcAdd_TestIntValue()
+        {
+            Calculator clc = new Calculator();
+
+            int retVal = clc.Add("1");
+            int constIntOne = 1;
+            Assert.AreEqual(retVal, constIntOne, "Add calculator for empty String");
+
+        }
+        [TestMethod]
+        public void CalcAdd_TestCommaSeperatedValue()
+        {
+            Calculator clc = new Calculator();
+
+            int retVal = clc.Add("1,2");
+            const int constIntTotal = 3;
+            Assert.AreEqual(retVal, constIntTotal, "Add calculator for empty String");
+
         }
     }
 }
